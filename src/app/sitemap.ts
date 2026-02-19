b2c6fdf856b000b6dbd0c24eee7bb33f5722ba18
yusrakuloglu <yusrak.kuloglu@gmail.com>
2026-02-19 14:44:05 +0300

    add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,38 @@
+import { MetadataRoute } from "next";
+
+const BASE_URL = "https://yusrakuloglu.com";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: BASE_URL,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${BASE_URL}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/experience`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/projects`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/contact`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.6,
+    },
+  ];
+}
